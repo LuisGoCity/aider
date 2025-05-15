a7544f47da3dd4b63e044fe55ad5f80755eaf970
LuisGoCity <luis.lopez@gocity.com>
2025-05-15 14:51:43 +0100

    add methods to change statuses of jira tickets

--- aider/jira.py
+++ aider/jira.py
@@ -36,6 +36,8 @@ class Jira:
                 method=method,
                 url=url,
                 params=kwargs.get("params"),
+                data=kwargs.get("data"),
+                json=kwargs.get("json_body"),
                 headers=(
                     kwargs.get("json_header") if kwargs.get("json_header") else self.json_header
                 ),
@@ -44,8 +46,12 @@ class Jira:
         except Exception as e:
             raise Exception(f"Could not make {method} request to url: {url}.\n {e}")
 
-        if response.status_code != 200:
+        if response.status_code not in [200, 204]:
             raise Exception(f"{method} request for url {url} raised error: {response.status_code}")
+
+        if response.status_code == 204:
+            print("Request completed successfully but returned no content.")
+            return
 
         return json.loads(response.text)
 
@@ -91,3 +97,64 @@ class Jira:
             issue_content["comments"] = comments
 
         return issue_content
+
+    def get_status_options(self, project_id=None):
+        try:
+            response = self._make_request(method="GET", request_url="/status")
+        except Exception as e:
+            raise Exception(f"Could not retrieve status options for {self.base_url}:\n {e}")
+
+        if not project_id:
+            return response
+
+        return [
+            response_
+            for response_ in response
+            if response_.get("scope", {}).get("project", {}).get("id") == project_id
+        ]
+
+    def get_ticket_status_options(self, issue_key_or_id):
+        status_options = self._make_request(
+            method="GET", request_url=f"/issue/{issue_key_or_id}/transitions"
+        )
+        if not status_options.get("transitions"):
+            print(f"No status options available for issue {issue_key_or_id}")
+            return
+
+        return status_options["transitions"]
+
+    def set_ticket_to_review(self, issue_key_or_id):
+        ticket = self.get_issue(issue_key_or_id)
+        current_status = ticket["fields"]["status"]
+
+        if "review" in current_status["name"].lower():
+            print("Ticket already in review.")
+            return
+
+        status_options = self.get_ticket_status_options(issue_key_or_id)
+        to_status = [
+            status["id"] for status in status_options if "review" in status["name"].lower()
+        ]
+
+        if not to_status:
+            print(
+                "Could not find a review-like status in jira board for project"
+                f" {ticket['fields']['project']['name']}."
+            )
+            return
+        else:
+            to_status = to_status[0]
+
+        json_body = {"transition": to_status}
+        json_header = {"Accept": "application/json", "Content-Type": "application/json"}
+        try:
+            self._make_request(
+                method="POST",
+                request_url=f"/issue/{issue_key_or_id}/transitions",
+                json_body=json_body,
+                json_header=json_header,
+            )
+        except Exception as e:
+            raise Exception(f"Failed to update status field for issue {issue_key_or_id}:\n {e}")
+
+        print(f"Successfully updated status of issue {issue_key_or_id} to review.")
